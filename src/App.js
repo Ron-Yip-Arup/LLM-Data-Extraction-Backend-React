248814e1f9f79b938ad817a56ab8ab41cafe041b
Ron-Yip-Arup <ron.yip@arup.com>
2024-09-24 16:12:39 +0800

    feat: complete text extract from pdf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Data from './Data.js';
 import FileUploadButton from './FileUpload.js';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
   return (
     <div>
       <header>
-        <FileUploadButton/>
+        <FileUploadButton setData={setData}/>
       </header>
-      {/* <body>
-        <Data data_key="key1" data_value="value"/>
-      </body> */}
+      <body>
+        <p>{data}</p>
+        {/* <Data data_key="key1" data_value="value"/> */}
+      </body>
     </div>
   );
 }
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,21 +1,69 @@
 import React, { useState } from 'react';
-// import Data from './Data.js';
-// <Data data_key="key1" data_value="value"/>
 
+import * as pdfjsLib from "pdfjs-dist/webpack";
 
-const FileUploadButton = () => {
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
+
+function FileUploadButton ({ setData }) {
   const [uploadStatus, setUploadStatus] = useState('');
   const [jsonString, setJsonString] = useState('{}');
 
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    // You can perform further operations with the uploaded file here
-    console.log('Selected file:', file);
+  async function handleFileUpload(event) {
+    const file = event.target.files[0]
+    const reader = new FileReader();
+    var headerIndex = 7;
+    var footerIndex = 3;
+    var pageArray = new Array();
     
-    // Update the upload status
-    setUploadStatus('uploaded');
-    setJsonString(file.name)
-  };
+    reader.onload = async function() {
+      try {
+        const pdf = await pdfjsLib.getDocument(reader.result).promise;
+        for (var i = 1; i <= pdf.numPages; i++) {
+          let phraseArray = new Array();
+          
+          const page = await pdf.getPage(i);
+          const textContent = await page.getTextContent();
+          for (const element of textContent.items) {
+            if (!/^\s*$/.test(element.str)) {
+              phraseArray.push({ str: element.str, x: parseInt(element.transform[4]), y: parseInt(element.transform[5]), height: element.height });
+            }
+          }
+          const isSameLine = (a, b) => !(Math.abs(b.y - a.y) > (a.height + b.height) / 4);
+          phraseArray.sort((a, b) => isSameLine(a, b) ? a.x - b.x: b.y - a.y);
+          
+          let lineArray = new Array();
+          let currentLine = phraseArray[0].str;
+          for (let i = 1; i < phraseArray.length; i++) {
+            if (isSameLine(phraseArray[i-1], phraseArray[i])) {
+              currentLine = currentLine + " | " + phraseArray[i].str;
+            } else {
+              lineArray.push(currentLine);
+              currentLine = phraseArray[i].str;
+            }
+          }
+          lineArray.push(currentLine);
+          pageArray.push(lineArray);
+
+          
+        }
+
+        var pdfContent = pageArray[0].slice(0, headerIndex).join('\n') + '\n';
+        console.log(pageArray);
+        for (const lineArray of pageArray) {
+          pdfContent += lineArray.slice(headerIndex, lineArray.length - footerIndex).join('\n');
+          pdfContent += '\n';
+        }
+        pdfContent += pageArray[0].slice(pageArray[0].length - footerIndex, pageArray[0].length).join('\n');
+    
+        setData(pdfContent);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    reader.readAsArrayBuffer(file);
+    
+  }
 
   return (
     <div>
@@ -24,8 +72,6 @@ const FileUploadButton = () => {
         id="fileInput"
         onChange={handleFileUpload}
       />
-      
-      {uploadStatus === 'uploaded' && <p>{jsonString}</p>}
     </div>
   );
 };
